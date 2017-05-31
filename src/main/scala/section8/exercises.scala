54a56bd6830c102e1d178cf6acd97fcd7abe27fb
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-05-31 12:39:44 +0200

    Completed section 8 exercise

--- src/main/scala/section8/exercises.scala
+++ src/main/scala/section8/exercises.scala
@@ -1,10 +1,5 @@
 // Check for Single Even:
-def check_even(num:Int): Boolean={
-	if(num%2 == 0){
-		true
-	}
-	else false
-}
+def check_even(num:Int) = num%2 == 0
 
 // Check for Evens in a List:
 def check_list(x:List[Int]): Boolean={
@@ -30,6 +25,27 @@ def lucky_seven(x:List[Int]): Int={
 	sum
 }
 
-println(check_even(3))	
-println(check_list(List(1,3,11)))
-println(lucky_seven(List(1,7,10,14)))
+// Balance Check:
+def balance(mylist:List[Int]): Boolean = {
+  var firsthalf = 0
+  var secondhalf = mylist.sum
+
+  for(i <- Range(0, mylist.length)){
+    firsthalf = firsthalf + mylist(i)
+    secondhalf = secondhalf - mylist(i)
+
+    if(firsthalf == secondhalf){
+      return true
+    }
+  }
+  false
+}
+
+// Palindrome Check:
+def palindrome(word:String) = word == word.reverse
+
+//println(check_even(3))
+//println(check_list(List(1,3,11)))
+//println(lucky_seven(List(1,7,10,14)))
+//println(balance(List(2,3,3,3)))
+println(palindrome("maoam"))
